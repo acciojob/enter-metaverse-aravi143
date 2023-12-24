3e239610eb844357097007e7171166085a9a599f
Aravind Chinna <151151715+aravi143@users.noreply.github.com>
2023-12-24 17:02:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const button = document.getElementById('enterBtn');
+button.addEventListener('click',() =>{
+	const para = document.getElementById('status');
+	para.innerHTML ='<h1>Entered Metaverse</h1>'
+})
